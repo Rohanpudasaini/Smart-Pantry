9ea6f9f7361ae1fc467feb3a4020324d03d47768
Rohan <pudasainirohan0@gmail.com>
2025-11-21 00:08:54 +0545

    feat: add args to read message

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 
 from dotenv import load_dotenv
@@ -9,15 +10,30 @@ from utils.config import final_text_from_response
 load_dotenv()
 
 
+async def parse_arguments():
+    parser = argparse.ArgumentParser(description="Simple Gemini AI Agent")
+    parser.add_argument(
+        "positional_message", nargs="?", help="Message to send to the AI"
+    )
+
+    parser.add_argument(
+        "--message", "-m", type=str, help="The message to send to the Gemini AI model"
+    )
+
+    args = parser.parse_args()
+    message = args.message or args.positional_message
+    return message
+
+
 async def main():
     # Simulate the dynamic context (reading from your files)
+    default_message = "Please plan my meal, Today, I want something high in protein."
+    message = await parse_arguments() or default_message
 
     runner = InMemoryRunner(agent=manager_agent)
 
     print("🤖 Manager is thinking...")
-    response = await runner.run_debug(
-        "Please plan my meal, Today, I want something high in protein."
-    )
+    response = await runner.run_debug(message)
     final_text = await final_text_from_response(response)
     print(final_text)
 
--- templates/todays_meal_plan.md
+++ templates/todays_meal_plan.md
@@ -1,7 +1,7 @@
 ## Meal Plan:
 
 ### Today:
-**Easy Cheesy Scrambled Eggs**
-*   **Protein:** 25g
-*   **Fat:** 15g
-*   **Carbs:** 3g
+**Breakfast pizza with egg**
+*   **Protein:** 14.02g
+*   **Fat:** 14.5g
+*   **Carbs:** 25.63g
